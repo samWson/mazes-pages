4ff13ad0c8ba54b285ad9dac0fea555bd51a6911
Samuel Wilson <samuel.wilson@fluxfederation.com>
2019-08-06 16:58:41 +1200

    Allow cells to be linked to each other

---
+++ MazeGenerator/Models/Cell.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MazeGenerator.Models
+{
+    public class Cell
+    {
+        public List<Cell> Links { get; set; }
+
+        public Cell()
+        {
+            Links = new List<Cell>();
+        }
+
+        public void Link(Cell cell, bool bidirectional = false)
+        {
+            Links.Add(cell);
+
+            if (bidirectional)
+            {
+                cell.Link(this);
+            }
+        }
+    }
+}
---
+++ MazeGeneratorTest/CellTest.cs
@@ -0,0 +1,35 @@
+using System;
+using Xunit;
+using MazeGenerator.Models;
+
+namespace MazeGeneratorTest
+{
+    public class CellTest
+    {
+        private readonly Cell _firstCell;
+        private readonly Cell _secondCell;
+
+        public CellTest()
+        {
+            _firstCell = new Cell();
+            _secondCell = new Cell();
+        }
+
+        [Fact]
+        public void TestLink()
+        {
+            _firstCell.Link(_secondCell, true);
+
+            Assert.Collection<Cell>(
+                _firstCell.Links,
+                item => Assert.Equal<Cell>(_secondCell, item)
+                );
+
+            Assert.Collection<Cell>(
+                _secondCell.Links,
+                item => Assert.Equal<Cell>(_firstCell, item)
+                );
+        }
+        }
+    }
+}

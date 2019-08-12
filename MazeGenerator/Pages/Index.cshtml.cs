c1dceff22f9b1ad1b260249693e66da4b8d2372e
Samuel Wilson <samuel.wilson@fluxfederation.com>
2019-08-13 07:52:29 +1200

    Capture form input from POST request

--- MazeGenerator/Pages/Index.cshtml.cs
+++ MazeGenerator/Pages/Index.cshtml.cs
@@ -9,9 +9,11 @@ namespace MazeGenerator.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        public void OnPostGenerate(int rows, int columns)
         {
-
+            // FIXME: This method keeps submitting the last entered values from the form
+            // each time the page is refreshed.
+            Console.WriteLine($"\n\nNumber of rows: {rows}\nNumber of columns: {columns}\n\n");
         }
     }
 }

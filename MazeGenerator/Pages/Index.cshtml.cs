aff11244616a5705666dd1c9ad39a812ae7b95d7
Samuel Wilson <samuel.wilson@fluxfederation.com>
2019-08-17 21:43:37 +1200

    Expose property to allow generated maze to be displayed

--- MazeGenerator/Pages/Index.cshtml.cs
+++ MazeGenerator/Pages/Index.cshtml.cs
@@ -10,15 +10,18 @@ namespace MazeGenerator.Pages
 {
     public class IndexModel : PageModel
     {
+        [ViewData]
+        public string Maze { get; set; }
+
         // FIXME: This method keeps submitting the last entered values from the form
         // each time the page is refreshed.
         public void OnPostGenerate(int rows, int columns)
         {
-            Point extent = new Point(columns, rows);
-            Grid grid = new Grid(extent);
+            Grid grid = new Grid(new Point(columns, rows));
 
-            Console.WriteLine($"\n\nNumber of rows: {rows}\nNumber of columns: {columns}\n\n");
-            Console.WriteLine($"\n\n{grid.ToString()}\n\n");
+            // TODO: Eventually set this as `Maze = grid.ToString();`
+            Maze = $"This is where the maze goes. It is made of {rows} rows and {columns} columns.";
+
         }
     }
 }

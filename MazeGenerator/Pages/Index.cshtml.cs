bfb35eb7cf20060f6655b0f10769b099af010790
Samuel Wilson <samuel.wilson@fluxfederation.com>
2019-08-22 20:41:54 +1200

    Display a grid at the view as ASCII art
    An automatic code formatter changed all the white space in Grid.cs
    making it look like the entire file changed. The `ToString()`
    method and its helpers are the important change, allowing the grid
    to be represented as a string.
    
    The variables `i` and `j` used in `for` loops have been replaced
    with `x` and `y` as appropriate to aid in reading the code as a
    grid of cells acessed with x and y coordinates.

--- MazeGenerator/Pages/Index.cshtml.cs
+++ MazeGenerator/Pages/Index.cshtml.cs
@@ -8,20 +8,20 @@ using MazeGenerator.Models;
 
 namespace MazeGenerator.Pages
 {
-    public class IndexModel : PageModel
+  public class IndexModel : PageModel
+  {
+    [ViewData]
+    public string Maze { get; set; }
+
+    // FIXME: This method keeps submitting the last entered values from the form
+    // each time the page is refreshed.
+    public void OnPostGenerate(int rows, int columns)
     {
-        [ViewData]
-        public string Maze { get; set; }
+      Grid grid = new Grid(new Point(columns, rows));
 
-        // FIXME: This method keeps submitting the last entered values from the form
-        // each time the page is refreshed.
-        public void OnPostGenerate(int rows, int columns)
-        {
-            Grid grid = new Grid(new Point(columns, rows));
-
-            // TODO: Eventually set this as `Maze = grid.ToString();`
-            Maze = $"This is where the maze goes. It is made of {rows} rows and {columns} columns.";
-
-        }
+      // TODO: Eventually set this as `Maze = grid.ToString();`
+      Maze = $"This is where the maze goes. It is made of {rows} rows and {columns} columns.\n\n";
+      Maze += grid.ToString();
     }
+  }
 }

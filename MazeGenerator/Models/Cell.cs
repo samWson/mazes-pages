21bb4b5de4d4ddd183a2fcb637e0361905bab068
Samuel Wilson <samuel.wilson@fluxfederation.com>
2019-08-23 17:58:55 +1200

    Whitespace change by automatic formatter
    Formatter is VSCode csharp plugin.

--- MazeGenerator/Models/Cell.cs
+++ MazeGenerator/Models/Cell.cs
@@ -2,37 +2,37 @@
 
 namespace MazeGenerator.Models
 {
-    public class Cell
+  public class Cell
+  {
+    public int Row { get; }
+    public int Column { get; }
+    public List<Cell> Links { get; set; }
+    public Cell North { get; set; }
+    public Cell South { get; set; }
+    public Cell East { get; set; }
+    public Cell West { get; set; }
+
+    public Cell(Point coordinates)
     {
-        public int Row { get; }
-        public int Column { get; }
-        public List<Cell> Links { get; set; }
-        public Cell North { get; set; }
-        public Cell South { get; set; }
-        public Cell East { get; set; }
-        public Cell West { get; set; }
-
-        public Cell(Point coordinates)
-        {
-            Column = coordinates.X;
-            Row = coordinates.Y;
-            Links = new List<Cell>();
-        }
-
-        public void LinkBidirectionally(Cell cell)
-        {
-            this.Link(cell);
-            cell.Link(this);
-        }
-
-        public bool IsLinked(Cell cell)
-        {
-            return Links.Contains(cell);
-        }
-
-        private void Link(Cell cell)
-        {
-            Links.Add(cell);
-        }
+      Column = coordinates.X;
+      Row = coordinates.Y;
+      Links = new List<Cell>();
     }
+
+    public void LinkBidirectionally(Cell cell)
+    {
+      this.Link(cell);
+      cell.Link(this);
+    }
+
+    public bool IsLinked(Cell cell)
+    {
+      return Links.Contains(cell);
+    }
+
+    private void Link(Cell cell)
+    {
+      Links.Add(cell);
+    }
+  }
 }
--- MazeGeneratorTest/CellTest.cs
+++ MazeGeneratorTest/CellTest.cs
@@ -3,39 +3,39 @@ using MazeGenerator.Models;
 
 namespace MazeGeneratorTest
 {
-    public class CellTest
+  public class CellTest
+  {
+    private readonly Cell _firstCell;
+    private readonly Cell _secondCell;
+
+    public CellTest()
     {
-        private readonly Cell _firstCell;
-        private readonly Cell _secondCell;
-
-        public CellTest()
-        {
-            _firstCell = new Cell(new Point(1, 1));
-            _secondCell = new Cell(new Point(2, 1));
-        }
-
-        [Fact]
-        public void TestLink()
-        {
-            _firstCell.LinkBidirectionally(_secondCell);
-
-            Assert.Collection<Cell>(
-                _firstCell.Links,
-                item => Assert.Equal<Cell>(_secondCell, item)
-                );
-
-            Assert.Collection<Cell>(
-                _secondCell.Links,
-                item => Assert.Equal<Cell>(_firstCell, item)
-                );
-        }
-
-        [Fact]
-        public void TestIsLinked()
-        {
-            _firstCell.LinkBidirectionally(_secondCell);
-
-            Assert.True(_firstCell.IsLinked(_secondCell));
-        }
+      _firstCell = new Cell(new Point(1, 1));
+      _secondCell = new Cell(new Point(2, 1));
     }
+
+    [Fact]
+    public void TestLink()
+    {
+      _firstCell.LinkBidirectionally(_secondCell);
+
+      Assert.Collection<Cell>(
+          _firstCell.Links,
+          item => Assert.Equal<Cell>(_secondCell, item)
+          );
+
+      Assert.Collection<Cell>(
+          _secondCell.Links,
+          item => Assert.Equal<Cell>(_firstCell, item)
+          );
+    }
+
+    [Fact]
+    public void TestIsLinked()
+    {
+      _firstCell.LinkBidirectionally(_secondCell);
+
+      Assert.True(_firstCell.IsLinked(_secondCell));
+    }
+  }
 }
